dc5fb8a6f8ef9ee7481fab802e0d74575fc5f0c4
atsutko <atsushi.takao.66@gmail.com>
2018-10-21 16:52:20 +0900

    Initial Commit

---
+++ RealmPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by TakaoAtsushi on 2018/10/21.
+//  Copyright © 2018 TakaoAtsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
